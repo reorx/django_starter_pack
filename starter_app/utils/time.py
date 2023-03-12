111b4c5dd27a8ed69eacb053396772ca5eeb87ae
Reorx <novoreorx@gmail.com>
2023-03-12 23:49:59 +0800

    update time in utils

--- starter_app/utils/time.py
+++ starter_app/utils/time.py
@@ -1,6 +1,8 @@
+import datetime
 import arrow
 from ..settings import TIME_ZONE
 
+# arrow #
 
 def format_time(dt):
     return arrow.get(dt).to(TIME_ZONE).format('YYYY-MM-DD HH:mm:ss')
@@ -12,3 +14,24 @@ def format_time_tz(dt):
 
 def format_time_zh(dt):
     return arrow.get(dt).to(TIME_ZONE).format('YYYY年M月D日 HH:mm:ss')
+
+
+# datetime #
+
+tz = datetime.timezone(datetime.timedelta(hours=8))
+
+
+def get_now():
+    """
+    NOTE this function is not effected by TZ environment
+    """
+    now = datetime.datetime.now(tz=tz)
+    return now
+
+
+def get_today_str(fmt='%Y-%m-%d'):
+    return get_now().strftime(fmt)
+
+
+def get_utc_now_str():
+    return datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
