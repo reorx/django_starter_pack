129b112a2c9b1e886f2b887db131c2d1cf10e45b
Reorx <novoreorx@gmail.com>
2024-08-10 23:28:53 +0800

    move gen_frontend_types to project dir

---
+++ gen_frontend_types.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+PYTHONPATH=. ./venv/bin/python ./scripts/gen_frontend_types.py starter_app ./test/frontend
--- test/gen_frontend_types.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pushd ..
-PYTHONPATH=. ./venv/bin/python ./scripts/gen_frontend_types.py starter_app test/frontend
-popd
