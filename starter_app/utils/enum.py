7b7c4813f8edd1b61324ef31489dd02e48cf658f
Reorx <novoreorx@gmail.com>
2024-08-22 09:47:31 +0800

    update enum

--- starter_app/utils/enum.py
+++ starter_app/utils/enum.py
@@ -8,6 +8,7 @@ class SimpleEnumMeta(type):
     def __new__(mcs, name, bases, attrs):
         keys = []
         values = []
+        value_key_map = {}
         for k, v in attrs.items():
             if k.startswith('_'):
                 continue
@@ -16,8 +17,10 @@ class SimpleEnumMeta(type):
                 attrs[k] = v
             keys.append(k)
             values.append(v)
+            value_key_map[v] = k
         attrs['_keys'] = keys
         attrs['_values'] = values
+        attrs['_value_key_map'] = value_key_map
         return type.__new__(mcs, name, bases, attrs)
 
 
@@ -33,3 +36,7 @@ class SimpleEnum(with_metaclass(SimpleEnumMeta, object)):
     @classmethod
     def items(cls):
         return list(zip(cls._keys, cls._values))
+
+    @classmethod
+    def value_to_key(cls, value):
+        return cls._value_key_map[value]
